9f465e4c8339cce5a657ad9bc197340cf6afd771
diegosanchez <diego.sanchez@mercadolibre.com>
2017-03-10 16:16:00 -0300

    [Dojo#3] Notes

--- dojo_3/javascripts/test/solution.js
+++ dojo_3/javascripts/test/solution.js
@@ -1,8 +1,16 @@
 const chai = require('chai');
 
+const Bomber = require('../bomber');
+const Cell = require('../cell');
+const Something = require('../something');
+
 describe("bomberman", () => {
     describe("bomberman", () => {
-        it.skip("steps into a cell which is busy with an object then it doesn't move and throw an exception", () => {
+        it("steps into a cell which is empty then it moves in", () => {
+
+        });
+
+        it("steps into a cell which is busy with an object then it doesn't move and throw an exception", () => {
         });
 
         it.skip("steps into a cell which is busy with an enemy then it dies", () => {
