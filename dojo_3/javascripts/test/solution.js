09318e9bb762987971153e29a5b436564e16ef5b
diegosanchez <diego.sanchez@mercadolibre.com>
2017-03-23 17:22:22 -0300

    [dojo_3] Notes 2

--- dojo_3/javascripts/test/solution.js
+++ dojo_3/javascripts/test/solution.js
@@ -7,10 +7,21 @@ const Something = require('../something');
 describe("bomberman", () => {
     describe("bomberman", () => {
         it("steps into a cell which is empty then it moves in", () => {
+            var bornPlace = new Cell();
+            var bomber = new Bomber( bornPlace );
+            var here = new Cell();
+            bomber.stepsIn( here );
+            chai.assert.equal( bomber.isIn(here), true);
 
         });
 
         it("steps into a cell which is busy with an object then it doesn't move and throw an exception", () => {
+            var bornPlace = new Cell();
+            var bomber = new Bomber( bornPlace );
+            var here = new Cell( new Something() );
+            chai.assert.throws( () => {
+                bomber.stepsIn( here );
+            });
         });
 
         it.skip("steps into a cell which is busy with an enemy then it dies", () => {
